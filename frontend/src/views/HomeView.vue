92c3f84e6a181b5b0202105a3cbd605a084ce0fc
leticia borges asssunção <leticiabborges31@gmail.com>
2026-06-23 10:16:14 -0300

    fix: usa arquivo local cnpq.png

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -162,7 +162,7 @@
         <div class="logos-financiadores">
           <div class="logo-item">
             <img
-              src="https://portalpadrao.ufma.br/ineof/imagens/cnpq.png"
+               src="../assets/cnpq.png"
               alt="CNPq - Conselho Nacional de Desenvolvimento Científico e Tecnológico"
               class="logo-financiador cnpq"
             />

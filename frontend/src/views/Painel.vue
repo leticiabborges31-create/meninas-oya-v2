ec6d27eff250ca6c5f1bd1d7b5b464af30ea1c15
leticia borges asssunção <leticiabborges31@gmail.com>
2026-06-24 10:49:57 -0300

    trocar +aluno por +estudante

--- frontend/src/views/Painel.vue
+++ frontend/src/views/Painel.vue
@@ -350,7 +350,7 @@
               <template v-if="!mostrarFormAluno"><line x1="12" y1="5" x2="12" y2="19"/><line x1="5" y1="12" x2="19" y2="12"/></template>
               <template v-else><line x1="18" y1="6" x2="6" y2="18"/><line x1="6" y1="6" x2="18" y2="18"/></template>
             </svg>
-            {{ mostrarFormAluno ? 'Cancelar' : 'Novo Aluno' }}
+            {{ mostrarFormAluno ? 'Cancelar' : 'Estudante' }}
           </button>
         </div>
 
